9256decaf893314430a1fa8145d07416938e53b9
Joshua Sullivan <jsulliva@nerdery.com>
2017-01-10 13:54:52 -0600

    Renamed the “ViewController” class to “RootViewController”.

---
+++ BeyondViewControllers/views/ForecastCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastCollectionViewCell.swift
+//  BeyondViewControllers
+//
+//  Created by Joshua Sullivan on 1/7/17.
+//  Copyright © 2017 Josh Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ForecastCollectionViewCell: UICollectionViewCell {
+    
+}
